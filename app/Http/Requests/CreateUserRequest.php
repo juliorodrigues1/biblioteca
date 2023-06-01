475be3023e67e0fdb9fd12c4ec3b994a3facace3
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-06-01 10:17:10 -0500

    criando requeste para validar cadastro de usuário

---
+++ app/Http/Requests/CreateUserRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => 'required|string',
+            'email' => 'required|email|unique:usuarios,email',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'nome.required' => 'O campo nome é obrigatório',
+            'email.required' => 'O campo email é obrigatório',
+            'email.email' => 'O campo email deve ser um email válido',
+            'email.unique' => 'O campo email já está em uso',
+        ];
+    }
+}
