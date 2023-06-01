d88377a5ef20bbbe0fbd496aee7e4edb0402242a
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-06-01 10:43:40 -0500

    criando rota para buascar todos os usuários

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/users', [\App\Http\Controllers\UserController::class, 'store']);
 Route::get('/users/{id}', [\App\Http\Controllers\UserController::class, 'show']);
+Route::get('/users', [\App\Http\Controllers\UserController::class, 'index']);
